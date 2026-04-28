0ed9d42cce179b57a734789450a70d9ec974d947
DERK0CHER <andenmaus@outlook.com>
2026-04-28 12:39:47 +0200

    25.11

--- modules/boot.nix
+++ modules/boot.nix
@@ -8,16 +8,16 @@
   boot.initrd.supportedFilesystems = [ "zfs" ];
 
   # Required for ZFS imports at boot
-  networking.hostId = "3d6394da";
+  networking.hostId = "d1c53402";
 
   # EFI bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Encrypted swap with random key each boot
-  swapDevices = [
+  swapDevices = lib.mkForce [
     {
-      device = "/dev/disk/by-partuuid/9374bfc1-bcad-4a88-b0d2-71b0589cce1a";
+      device = "/dev/disk/by-id/ata-SAMSUNG_SSD_830_Series_S0WJNYABC08589-part3";
       randomEncryption.enable = true;
     }
   ];
